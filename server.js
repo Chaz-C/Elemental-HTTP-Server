323b1c152d61944a2e51ad345c7a586d870a40f2
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2017-01-03 22:31:33 -1000

    cleaned up code and finished

--- server.js
+++ server.js
@@ -10,17 +10,12 @@ const fileNotFoundErrorHandler = (res) => {
 };
 
 const sendContent = (res, content) => {
-  res.setHeader('Content-Type', 'text/html');
   res.write(content);
-  res.end('\nPeace out, yo\n');
+  res.end();
 };
 
 const server = http.createServer( (req, res) => {
   let theUrl = req.url.slice(1);
-  // console.log(req.url);
-  // console.log('req url', theUrl);
-  // console.log('req method', req.method);
-  // console.log('req headers', req.headers);
 
   if ( req.method === 'POST' && req.url === '/elements') {
     let reqBody = '';
@@ -47,7 +42,6 @@ const server = http.createServer( (req, res) => {
 </body>
 </html>`;
 
-
       fs.readdir('./public', (err, files) => {
         if (files.indexOf(`${bodyQS.elementName.toLowerCase()}.html`) < 0 ) {
           fs.writeFile(`./public/${bodyQS.elementName.toLowerCase()}.html`, `${newHTMLFile}`, (err) => {
@@ -70,8 +64,9 @@ const server = http.createServer( (req, res) => {
                 });
               }
             });
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 200;
+            res.writeHead(200, 'OK', {
+              'Content-Type': 'application/json'
+            });
             res.write('{ "success" : true }');
             res.end();
             return;
@@ -89,59 +84,32 @@ const server = http.createServer( (req, res) => {
       if (err) {
         fileNotFoundErrorHandler(res);
       } else {
+        res.setHeader('Content-Type', 'text/css');
         sendContent(res, data);
       }
     });
   } else {
     fs.readdir('./public', (err, files) => {
-      console.log(files);
       if ( files.indexOf(theUrl) > 0 ) {
         fs.readFile(`./public/${theUrl}`, {encoding: 'utf8'}, (err, data) => {
           if (err) {
             fileNotFoundErrorHandler(res);
           }
+          res.setHeader('Content-Type', 'text/html');
           sendContent(res, data);
-          console.log(data);
         });
       } else {
-        res.statusCode = 404;
-        res.write('Invalid Url\n');
-        res.end();
+        fs.readFile('./public/404.html', {encoding: 'utf8'}, (err, data) => {
+          if (err) {
+            fileNotFoundErrorHandler(res);
+          }
+          res.statusCode = 404;
+          sendContent(res, data);
+        });
       }
     });
   }
-
-
-
 });
-
-
-  // let reqBody = '';
-  // req.setEncoding('utf8');
-  // req.on('data', (chunk) => {
-  //   reqBody += chunk;
-  // });
-  // req.on('end', () => {
-  //   //reqBody is complete, handle request!
-  //   bodyQS = qs.parse(reqBody);
-  // let newHTMLFile = `<!DOCTYPE html>
-  // <html lang="en">
-  // <head>
-  //   <meta charset="UTF-8">
-  //   <title>The Elements - ${bodyQS.elementName}</title>
-  //   <link rel="stylesheet" href="/css/styles.css">
-  // </head>
-  // <body>
-  //   <h1>${bodyQS.elementName}</h1>
-  //   <h2>${bodyQS.elementSymbol}</h2>
-  //   <h3>Atomic number ${bodyQS.elementAtomicNumber}</h3>
-  //   <p>${bodyQS.elementDescription}</p>
-  //   <p><a href="/">back</a></p>
-  // </body>
-  // </html>`;
-
-
-
 
 server.listen(PORT, () => {
   console.log('server is listening on port', PORT);
